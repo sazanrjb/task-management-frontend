a5cbf2de47366c35f6dc4f794dab7ec4c5ee6777
sazanrjb <sazanrjb@users.noreply.github.com>
2021-07-20 08:24:56 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.e7c3996dc8e00c907b9a6a39554cf3dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a1959729e78be92a6d28ed371e31209",
+    "url": "https://sazanrjb.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "bc362c3f0c4ea093e125",
+    "url": "https://sazanrjb.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "ceb605a2293db9471671",
+    "url": "https://sazanrjb.github.io/task-management-frontend/static/js/2.05d1b383.chunk.js"
+  },
+  {
+    "revision": "bc362c3f0c4ea093e125",
+    "url": "https://sazanrjb.github.io/task-management-frontend/static/js/main.cd38bdcd.chunk.js"
+  },
+  {
+    "revision": "8aa77ca82e9437bea1c4",
+    "url": "https://sazanrjb.github.io/task-management-frontend/static/js/runtime~main.67ccb9ca.js"
+  }
+]);
